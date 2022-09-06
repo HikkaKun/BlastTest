bc5640745857c7d4ae8709a91759879e5c7e6d22
HomoHikka <halfideas31@gmail.com>
2022-09-06 20:33:26 +0300

    Update Utilities
    - fix bug when randomEnumKeys returns enum key name instead of value

--- assets/scripts/Utilities.ts
+++ assets/scripts/Utilities.ts
@@ -1,6 +1,8 @@
-export function randomEnumKey<T>(anEnum: T): T[keyof T] {
-	const values = (Object.values(anEnum) as unknown) as T[keyof T][];
-	const index = Math.floor(Math.random() * values.length);
+export function randomEnumKey<T extends object>(anEnum: T): T[keyof T] {
+	const values = Object.keys(anEnum)
+		.map(n => Number.parseInt(n))
+		.filter(n => !Number.isNaN(n)) as unknown as T[keyof T][]
+	const index = Math.floor(Math.random() * values.length)
 
 	return values[index];
 }
