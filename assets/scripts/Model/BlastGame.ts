44f60f3ba80f19170e24a0115b3f41d87cbcbd5c
HomoHikka <halfideas31@gmail.com>
2022-09-06 16:30:37 +0300

    Update BlastGame
    - add width & height getters
    - add Tile's field
    - add initField function
    - add tileAt function
    - add checkBounds function

--- assets/scripts/Model/BlastGame.ts
+++ assets/scripts/Model/BlastGame.ts
@@ -1,3 +1,5 @@
+import Tile from './Tile';
+
 export interface BlastGameConfig {
 	width: number;
 	height: number;
@@ -12,7 +14,43 @@ export interface BlastGameConfig {
 }
 
 export default class BlastGame {
-	constructor(config: BlastGameConfig) {
 
+	public get width() {
+		return this._width;
+	}
+
+	public get height() {
+		return this._height;
+	}
+
+	private _width;
+	private _height;
+	private _field: Tile[];
+
+	constructor(config: BlastGameConfig) {
+		this._field = new Array<Tile>();
+		this._field.length = config.width * config.height;
+		this._width = config.width;
+		this._height = config.height;
+
+		this.initField();
+	}
+
+	public initField(): void {
+		const field = this._field;
+
+		for (let i = 0; i < field.length; i++) {
+			field[i] = new Tile();
+		}
+	}
+
+	public tileAt(x: number, y: number): Tile | null {
+		if (!this.checkBounds(x, y)) return null;
+
+		return this._field[x % this._width + y * this._height];
+	}
+
+	public checkBounds(x: number, y: number): Boolean {
+		return x > 0 && x < this.width && y > 0 && y < this.height;
 	}
 }
