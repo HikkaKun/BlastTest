d7915f4a544ba3b2a32a6c2a0bb9f68c99b69ec8
HomoHikka <halfideas31@gmail.com>
2022-09-08 13:29:18 +0300

    Add TileView spawn on scene
    - update OnGenerateTile callback
    - update block color mapping in editor

--- assets/scripts/Game/View.ts
+++ assets/scripts/Game/View.ts
@@ -1,6 +1,7 @@
 import BlockConfig from './BlockConfig';
 import BlastGame, { BlastGameConfig, BlastGameCallbacks, Position } from '../Model/BlastGame';
 import Color from '../Model/Color';
+import TileView from './TileView';
 
 const { ccclass, property } = cc._decorator;
 
@@ -9,9 +10,14 @@ export default class View extends cc.Component {
 	@property([BlockConfig])
 	public blockConfigs: Array<BlockConfig> = [];
 
+	@property(cc.Prefab)
+	public tileViewPrefab: cc.Prefab = null;
+
 	public blocks = new Map<Color, cc.SpriteFrame>;
 
 	public game!: BlastGame;
+
+	public tiles = new Map<string, TileView>;
 
 	public onLoad() {
 		for (const blockConfig of this.blockConfigs) {
@@ -31,6 +37,7 @@ export default class View extends cc.Component {
 		};
 
 		this.game = new BlastGame(config, callbacks);
+		this.game.initField();
 	}
 
 	private OnDestroyTile(position: Position) {
@@ -38,7 +45,20 @@ export default class View extends cc.Component {
 	}
 
 	private OnGenerateTile(forPosition: Position, fromOutside: boolean) {
+		const node = cc.instantiate(this.tileViewPrefab);
+		node.parent = this.node;
 
+		const view = node.getComponent(TileView);
+		view.id = forPosition.toString();
+
+		const sprite = node.getComponent(cc.Sprite);
+		sprite.spriteFrame = this.blocks.get(this.game.tileAt(forPosition.x, forPosition.y)?.color as Color) as cc.SpriteFrame;
+
+		node.x = forPosition.x * 40;
+		node.y = forPosition.y * 40;
+
+		node.width = 40;
+		node.height = 40;
 	}
 
 	private OnMoveTile(oldPosition: Position, newPosition: Position) {
