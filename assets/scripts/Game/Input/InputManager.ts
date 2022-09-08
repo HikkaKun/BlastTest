51058fd09c5c1f4aa2062a02c82ebd148bcaa7b7
HomoHikka <halfideas31@gmail.com>
2022-09-08 18:59:29 +0300

    Fix InputManager
    - fix bug when OnInput calls onDown instead of onMove & onUp
    - fix no handling events

--- assets/scripts/Game/Input/InputManager.ts
+++ assets/scripts/Game/Input/InputManager.ts
@@ -3,7 +3,7 @@ import IInputCommand from './Commands/IInputCommand';
 import TileInputCommand from './Commands/TileInputCommand';
 import InputCatcher from './InputCatcher';
 import { InputDirection, InputDirectionEnum } from './InputDirection';
-import InputType from './InputType';
+import { InputType, InputTypeEnum } from './InputType';
 
 const { ccclass, property } = cc._decorator;
 
@@ -12,6 +12,7 @@ export default class InputManager extends cc.Component {
 	private _inputCommands = new Map<InputDirectionEnum, IInputCommand>;
 
 	protected onLoad(): void {
+		this._handleEvents(true);
 		this._inputCommands.set(InputDirection.Tile, new TileInputCommand());
 	}
 
@@ -21,16 +22,16 @@ export default class InputManager extends cc.Component {
 		cc.systemEvent[func](GameEvent.Input, this.OnInput, this);
 	}
 
-	OnInput(type: , touch: cc.Touch, direction: InputDirectionEnum, inputCatcher: InputCatcher) {
+	OnInput(type: InputTypeEnum, touch: cc.Touch, direction: InputDirectionEnum, inputCatcher: InputCatcher) {
 		switch (type) {
 			case InputType.Down:
 				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
 				break;
 			case InputType.Move:
-				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
+				this._inputCommands.get(direction)?.onMove(touch, inputCatcher)
 				break;
 			case InputType.Up:
-				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
+				this._inputCommands.get(direction)?.onUp(touch, inputCatcher)
 				break;
 		}
 	}
