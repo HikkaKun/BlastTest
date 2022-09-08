61c2dba82ce39cca2207206b422bd1b18b6a95cd
HomoHikka <halfideas31@gmail.com>
2022-09-08 17:59:14 +0300

    Add InputManager
    - add InputManager to handle wich class handles Input event

---
+++ assets/scripts/Game/Input/InputManager.ts
@@ -0,0 +1,37 @@
+import GameEvent from '../GameEvent';
+import IInputCommand from './Commands/IInputCommand';
+import TileInputCommand from './Commands/TileInputCommand';
+import InputCatcher from './InputCatcher';
+import { InputDirection, InputDirectionEnum } from './InputDirection';
+import InputType from './InputType';
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class InputManager extends cc.Component {
+	private _inputCommands = new Map<InputDirectionEnum, IInputCommand>;
+
+	protected onLoad(): void {
+		this._inputCommands.set(InputDirection.Tile, new TileInputCommand());
+	}
+
+	private _handleEvents(isOn: boolean) {
+		const func = isOn ? 'on' : 'off';
+
+		cc.systemEvent[func](GameEvent.Input, this.OnInput, this);
+	}
+
+	OnInput(type: , touch: cc.Touch, direction: InputDirectionEnum, inputCatcher: InputCatcher) {
+		switch (type) {
+			case InputType.Down:
+				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
+				break;
+			case InputType.Move:
+				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
+				break;
+			case InputType.Up:
+				this._inputCommands.get(direction)?.onDown(touch, inputCatcher)
+				break;
+		}
+	}
+}
