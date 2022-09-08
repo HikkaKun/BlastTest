a2be128d930d5fb12469e9eb7a1744296ee0794f
HomoHikka <halfideas31@gmail.com>
2022-09-08 13:06:27 +0300

    Update BlastGame
    - rename Node class to Position

--- assets/scripts/Model/BlastGame.ts
+++ assets/scripts/Model/BlastGame.ts
@@ -16,12 +16,12 @@ export interface BlastGameConfig {
 }
 
 export interface BlastGameCallbacks {
-	OnDestroyTile: (position: Node) => void;
-	OnMoveTile: (oldPosition: Node, newPosition: Node) => void;
-	OnGenerateTile: (forPosition: Node, fromOutside: boolean) => void;
+	OnDestroyTile: (position: Position) => void;
+	OnMoveTile: (oldPosition: Position, newPosition: Position) => void;
+	OnGenerateTile: (forPosition: Position, fromOutside: boolean) => void;
 }
 
-export class Node {
+export class Position {
 	public readonly x: number;
 	public readonly y: number;
 
@@ -59,9 +59,9 @@ export default class BlastGame {
 	private _minTileGroupSize: number;
 	private _colors: number;
 
-	private OnDestroyTile: (position: Node) => void;
-	private OnMoveTile: (oldPosition: Node, newPosition: Node) => void;
-	private OnGenerateTile: (forPosition: Node, fromOutside: boolean) => void;
+	private OnDestroyTile: (position: Position) => void;
+	private OnMoveTile: (oldPosition: Position, newPosition: Position) => void;
+	private OnGenerateTile: (forPosition: Position, fromOutside: boolean) => void;
 
 	constructor(config: BlastGameConfig, callbacks: BlastGameCallbacks) {
 		this._field = new Array<Tile>();
@@ -82,13 +82,13 @@ export default class BlastGame {
 		return x % this._width + y * this._height;
 	}
 
-	public positionFromIndex(index: number): Node | null {
+	public positionFromIndex(index: number): Position | null {
 		const x = index % this.width;
 		const y = Math.floor(index / this.height);
 
 		if (!this.checkBounds(x, y)) return null;
 
-		return new Node(x, y);
+		return new Position(x, y);
 	}
 
 	public initField(): void {
@@ -96,7 +96,7 @@ export default class BlastGame {
 
 		for (let i = 0; i < field.length; i++) {
 			field[i] = this._generateTile();
-			this.OnGenerateTile(this.positionFromIndex(i) as Node, true);
+			this.OnGenerateTile(this.positionFromIndex(i) as Position, true);
 		}
 	}
 
@@ -121,22 +121,22 @@ export default class BlastGame {
 		this._destroyTiles(tiles);
 	}
 
-	public findGroup(x: number, y: number): Array<Node> {
+	public findGroup(x: number, y: number): Array<Position> {
 		if (!this.checkBounds(x, y)) return [];
 
-		const tiles = new Array<Node>;
+		const tiles = new Array<Position>;
 
 		const pickedColor = this.tileAt(x, y)?.color as Color;
-		const start = new Node(x, y);
+		const start = new Position(x, y);
 
-		const frontier = new Array<Node>;
+		const frontier = new Array<Position>;
 		frontier.push(start)
 		const visited = new Set<string>;
 		visited.add(start.toString());
 		tiles.push(start);
 
 		while (frontier.length) {
-			const current = frontier.shift() as Node;
+			const current = frontier.shift() as Position;
 			const neighbors = this._getNeighbors(current.x, current.y)
 			for (const next of neighbors) {
 				if (this.tileAt(next.x, next.y)?.color != pickedColor) continue;
@@ -151,8 +151,8 @@ export default class BlastGame {
 		return tiles;
 	}
 
-	private _getNeighbors(x: number, y: number): Array<Node> {
-		const neighbors = new Array<Node>;
+	private _getNeighbors(x: number, y: number): Array<Position> {
+		const neighbors = new Array<Position>;
 
 		const indexX = [0, 1, 0, -1];
 		const indexY = [-1, 0, 1, 0];
@@ -163,8 +163,8 @@ export default class BlastGame {
 
 			if (!this.tileAt(neighborX, neighborY)) continue;
 
-			const node = new Node(neighborX, neighborY);
-			neighbors.push(node);
+			const position = new Position(neighborX, neighborY);
+			neighbors.push(position);
 		}
 
 		return neighbors;
@@ -174,7 +174,7 @@ export default class BlastGame {
 		return new Tile({ color: randomEnumKey(Color, this._colors) });
 	}
 
-	private _destroyTiles(tiles: Array<Node>): void {
+	private _destroyTiles(tiles: Array<Position>): void {
 		const columnsToUpdate = new Set<number>;
 
 		for (let i = 0; i < tiles.length; i++) {
@@ -222,7 +222,7 @@ export default class BlastGame {
 				this._field[index] = tiles.shift() as Tile;
 			} else {
 				this._field[index] = this._generateTile();
-				this.OnGenerateTile(this.positionFromIndex(index) as Node, true);
+				this.OnGenerateTile(this.positionFromIndex(index) as Position, true);
 			}
 
 			y--;
