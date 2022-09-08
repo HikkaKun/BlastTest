98c482ded6ff5a51df58d6300bdbc1d5d413dbf9
HomoHikka <halfideas31@gmail.com>
2022-09-08 23:16:25 +0300

    Revert "Revert "Update enums""
    This reverts commit 2f6cf2860ab2a9e0ad27d4b8e5cd8992a69fb972.

--- assets/scripts/Game/GameEvent.ts
+++ assets/scripts/Game/GameEvent.ts
@@ -1,7 +1,13 @@
-export default cc.Enum({
+import { EnumType } from '../Utilities';
+
+const GameEvent = cc.Enum({
 	None: 0,
 
 	Input: 5,
 
 	TileTap: 10,
 });
+
+export default GameEvent;
+
+export type GameEventEnum = EnumType<typeof GameEvent>;
--- assets/scripts/Game/Input/InputType.ts
+++ assets/scripts/Game/Input/InputType.ts
@@ -1,7 +1,9 @@
+import { EnumType } from '../../Utilities';
+
 export const InputType = cc.Enum({
 	Down: 0,
 	Move: 1,
 	Up: 2
 });
 
-export type InputTypeEnum = typeof InputType[keyof typeof InputType]; 
+export type InputTypeEnum = EnumType<typeof InputType>;
