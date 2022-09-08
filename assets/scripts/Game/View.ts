9df298f54d76827c76c5c5f08371244297d284ca
HomoHikka <halfideas31@gmail.com>
2022-09-08 13:11:23 +0300

    Add Game initialization
    - add color mapping for sprites
    - add game callbacks
    - add game config

--- assets/scripts/Game/View.ts
+++ assets/scripts/Game/View.ts
@@ -1,5 +1,6 @@
 import BlockConfig from './BlockConfig';
-import BlastGame, { BlastGameConfig } from '../Model/BlastGame';
+import BlastGame, { BlastGameConfig, BlastGameCallbacks, Position } from '../Model/BlastGame';
+import Color from '../Model/Color';
 
 const { ccclass, property } = cc._decorator;
 
@@ -8,5 +9,39 @@ export default class View extends cc.Component {
 	@property([BlockConfig])
 	public blockConfigs: Array<BlockConfig> = [];
 
+	public blocks = new Map<Color, cc.SpriteFrame>;
 
+	public game!: BlastGame;
+
+	public onLoad() {
+		for (const blockConfig of this.blockConfigs) {
+			this.blocks.set(blockConfig.color as Color, blockConfig.spriteFrame);
+		}
+
+		const config: BlastGameConfig = {
+			width: 9,
+			height: 9,
+			winScore: 25,
+			turnsNumber: 10
+		};
+		const callbacks: BlastGameCallbacks = {
+			OnDestroyTile: (position) => this.OnDestroyTile(position),
+			OnGenerateTile: (forPosition, fromOutside) => this.OnGenerateTile(forPosition, fromOutside),
+			OnMoveTile: (oldPosition, newPosition) => this.OnMoveTile(oldPosition, newPosition)
+		};
+
+		this.game = new BlastGame(config, callbacks);
+	}
+
+	private OnDestroyTile(position: Position) {
+
+	}
+
+	private OnGenerateTile(forPosition: Position, fromOutside: boolean) {
+
+	}
+
+	private OnMoveTile(oldPosition: Position, newPosition: Position) {
+
+	}
 }
