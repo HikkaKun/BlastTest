6692e1c9632ed1e0ff4a7a2f23e62f909af4db0f
HomoHikka <halfideas31@gmail.com>
2022-09-08 19:03:26 +0300

    Add TileView input
    - add emitting event to TileInputCommand
    - add TileTap event to GameEvents

--- assets/scripts/Game/GameEvent.ts
+++ assets/scripts/Game/GameEvent.ts
@@ -2,4 +2,6 @@ export default cc.Enum({
 	None: 0,
 
 	Input: 5,
+
+	TileTap: 10,
 });
--- assets/scripts/Game/Input/Commands/TileInputCommand.ts
+++ assets/scripts/Game/Input/Commands/TileInputCommand.ts
@@ -1,5 +1,10 @@
+import GameEvent from '../../GameEvent';
+import TileView from '../../TileView';
+import InputCatcher from '../InputCatcher';
 import IInputCommand from './IInputCommand';
 
 export default class TileInputCommand extends IInputCommand {
-
+	onDown(touch: cc.Touch, InputCatcher: InputCatcher): void {
+		cc.systemEvent.emit(GameEvent.TileTap, InputCatcher.node.getComponent(TileView));
+	}
 }
