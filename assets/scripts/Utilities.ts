820714c744fc8c0e68105d9a4c230b4d7ef0ffaa
HomoHikka <halfideas31@gmail.com>
2022-09-09 14:01:27 +0300

    Update Utilities
    - add array shuffle utility function

--- assets/scripts/Utilities.ts
+++ assets/scripts/Utilities.ts
@@ -9,3 +9,16 @@ export function randomEnumKey<T extends object>(anEnum: T, limitTo?: number): T[
 
 export type EnumType<T> = T[keyof T];
 
+export function shuffleArray(array: Array<unknown>) {
+	let currentIndex = array.length, randomIndex;
+
+	while (currentIndex != 0) {
+
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
