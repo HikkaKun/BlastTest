6ee78484b178b77baf511c42b036396638091d9b
HomoHikka <halfideas31@gmail.com>
2022-09-09 14:07:31 +0300

    Update BlastGame
    - tile now has index field
    - config is no more optional

--- assets/scripts/Model/Tile.ts
+++ assets/scripts/Model/Tile.ts
@@ -3,12 +3,15 @@ import Color from './Color'
 
 export interface TileConfig {
 	color?: Color;
+	index: number;
 }
 
 export default class Tile {
 	public color: Color;
+	public index: number;
 
-	constructor(config?: TileConfig) {
+	constructor(config: TileConfig) {
 		this.color = config?.color ?? randomEnumKey(Color);
+		this.index = config.index;
 	}
 }
