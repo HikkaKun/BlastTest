e052fa1c8a9f7a02b09fd7072ef9818f83148644
HomoHikka <halfideas31@gmail.com>
2022-09-11 11:20:35 +0300

    Add Toggle
    - add Toggle component to easily disable/enable node

---
+++ assets/scripts/Game/Ui/Toggle.ts
@@ -0,0 +1,51 @@
+import GameEvent, { GameEventEnum } from '../GameEvent';
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Toggle extends cc.Component {
+	@property()
+	protected isActiveOnStart = false;
+
+	@property({ type: GameEvent })
+	public event: GameEventEnum = GameEvent.None;
+
+	@property()
+	public onlyOpacity = false;
+
+	@property()
+	public invertBool = false;
+
+	protected _isActive = false;
+	protected _tween: cc.Tween | null = null;
+
+	protected onLoad(): void {
+		this._isActive = this.isActiveOnStart;
+		if (!this._isActive) {
+			this.node.active = this.onlyOpacity;
+			this.node.opacity = 0;
+
+			cc.systemEvent.on(this.event, this.OnToggle, this);
+		}
+	}
+
+	public OnToggle(isOn = false, time = 0.5) {
+		if (this.invertBool) {
+			isOn = !isOn;
+		}
+
+		if (isOn != this._isActive) {
+			this._isActive = isOn;
+
+			if (isOn) {
+				this.node.active = true;
+			}
+
+			this._tween && this._tween.stop();
+			this._tween = cc.tween(this.node)
+				.to(time, { opacity: isOn ? 255 : 0 })
+				.call(() => { this.node.active = isOn || this.onlyOpacity })
+				.start();
+		}
+	}
+}
